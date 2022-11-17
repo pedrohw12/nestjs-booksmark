4362d7a530102690cda6c0b8ce9e2131989859b5
Pedro Wanderley <wanderley.pedro@outlook.com>
2022-11-17 12:26:22 -0300

    test: signup, sign

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
+import { GetUser } from '../../src/auth/decorator';
+import { JwtGuard } from '../../src/auth/guard';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-  @UseGuards(AuthGuard('jwt'))
   @Get('me')
-  getMe() {
-    return 'user info';
+  getMe(@GetUser() user: User) {
+    return user;
   }
 }
